0f913310e4d615ddeac757c66ce707f2bcd068dd
Jude14 <soueidjude@gmail.com>
2024-04-29 17:11:51 +0300

    feature: added about page content

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -8,67 +8,46 @@ const About = () => {
       <div className='aboutslogan'><img className='aboutpic' src={require("../pictures/port-img.avif")} alt="" />
         <h1 className='slogansentence'>Efficiency in Motion: Powering Tomorrow's Supply Chains</h1>
       </div>
-      <h1>test</h1></div>*/
+      <h1></h1>
+      <h1></h1>
+      <h1></h1>
+      </div>
     <div className='about' onLoad={window.scrollTo(0, 0)}>
-      <div className='slogan'>
+      {/* <div className='slogan'>
         <img
           className='bgpic'
           src={require('../pictures/scm.avif')}
           alt=''
         />
         <h1 className='slogansentence'>About Us</h1>
+      </div> */}
+      <div className="about-page">
+      <div className="about-section">
+        <img src={require("../pictures/office-pic.avif")} alt="Picture 1" className="about-image-left" />
+        <p className="about-text">Track employee performance and assigned tasks</p>
       </div>
-      <div className='about2'>
-        <h1 className='sentence2'>"Find Everything You Need & More"</h1>
-        <p>Our Shop offers the best deals on bikes & accessories.</p>
-        <p>
-          We will deliver your order on your doorstep! With only one click,
-          place your order now.
-        </p>
+      <div className="about-section">
+        <img src={require("../pictures/ofice-guy.avif")} alt="Picture 2" className="about-image-right" />
+        <p className="about-text">Make resource management one click away</p>
       </div>
-      <div className='footer'>
-        <div className='footer1'>
-          <img
-            className='lebpic'
-            src={require('../pictures/scm.avif')}
-            alt=''
-          />
-          <h2 className='footer1sub1'>Lebanon</h2>
-          <p2 className='footer1sub2'>
-            Lebanon represents our main shop, where we originally began{' '}
-          </p2>
-          <p2 className='footer1sub3'>
-            Originally started by Mr.Zakhia Tayeh, who soon sold half the shop
-            to{' '}
-          </p2>
-          <p2 className='footer1sub4'>
-            Mr. Jude Soueid in 1990, the co-founder of PedalNation
-          </p2>
-        </div>
-
-        <div className='footer2'>
-          <img
-            className='italypic'
-            src={require('../pictures/scm.avif')}
-            alt=''
-          />
-          <h2 className='footer2sub1'>Italy</h2>
-          <p2 className='footer2sub2'>
-            Our Second Shop is located in Italy, Roma.{' '}
-          </p2>
-          <p2 className='footer2sub3'>
-            Managed by the shop's third and newest partner{' '}
-          </p2>
-          <p2 className='footer2sub4'>Mr. Anthony Fadel since 2010.</p2>
-        </div>
+      </div>
+      <div className="about-page">
+      <div className="about-section">
+        <img src={require("../pictures/kpi-pic.avif")} alt="Picture 1" className="about-image-left" />
+        <p className="about-text">Visualize key aspects of your factory</p>
+      </div>
+      <div className="about-section">
+        <img src={require("../pictures/factory-pic.avif")} alt="Picture 2" className="about-image-right" />
+        <p className="about-text">Get Real Time data of your factory</p>
+      </div>
       </div>
 
-      <body>
-        <div class='footer-dark'>
+      <div>
+        <div className='footer-dark'>
           <footer>
-            <div class='container'>
-              <div class='row'>
-                <div class='one'>
+            <div className='container'>
+              <div className='row'>
+                {/* <div className='one'>
                   <h3>Services</h3>
                   <ul>
                     <li>
@@ -81,8 +60,8 @@ const About = () => {
                       <a href='#one'>maintenance</a>
                     </li>
                   </ul>
-                </div>
-                <div class='two'>
+                </div> */}
+                <div className='two'>
                   <h3>Contact</h3>
                   <ul>
                     <li>
@@ -97,14 +76,14 @@ const About = () => {
                   </ul>
                 </div>
               </div>
-              <p class='copyright'>
+              <p className='copyright'>
                 <p className='blogo1'>Pedal</p>
                 <p className='blogo2'>Nation</p> © 2022
               </p>
             </div>
           </footer>
         </div>
-      </body>
+      </div>
     </div>
     </>
   )
